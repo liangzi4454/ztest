5488e55ae148b29da8b61d164fdd6743e6d35c0e
liangzi4454 <1026163977@qq.com>
2015-02-06 12:01:46 +0800

    update code ssss

--- src/com/liangzi4454/ztest/util/Test.java
+++ src/com/liangzi4454/ztest/util/Test.java
@@ -3,8 +3,5 @@ package com.liangzi4454.ztest.util;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("测试git和gitHub");
-		System.out.println("OK");
-		System.out.println("123");
-		System.out.println("OK");
 	}
 }
